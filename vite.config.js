901a3250ac4efd815ec2155780d3b5ebfcc64332
Elvisthegreat <amadinelvis6@gmail.com>
2025-01-03 16:33:12 +0100

    Fix configuration for Heroku deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3002,
+    port: 3002,  // This is for local dev only
     open: true,
   },
+  base: '/',  // This is the key setting for production deployment
 });
